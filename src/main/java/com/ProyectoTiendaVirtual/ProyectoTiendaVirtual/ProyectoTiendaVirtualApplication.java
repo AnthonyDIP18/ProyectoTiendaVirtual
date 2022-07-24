2723ded9db966b3e351b42ea23b9dd86c2cca5a9
Anthony <Anthony@LAPTOP-64HN7J34>
2022-07-23 19:36:49 -0600

    Proyecto 40%

---
+++ src/main/java/com/ProyectoTiendaVirtual/ProyectoTiendaVirtual/ProyectoTiendaVirtualApplication.java
@@ -0,0 +1,16 @@
+package com.ProyectoTiendaVirtual.ProyectoTiendaVirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoTiendaVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoTiendaVirtualApplication.class, args);
+                
+                
+                
+	}
+
+}
---
+++ src/test/java/com/ProyectoTiendaVirtual/ProyectoTiendaVirtual/ProyectoTiendaVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProyectoTiendaVirtual.ProyectoTiendaVirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoTiendaVirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
