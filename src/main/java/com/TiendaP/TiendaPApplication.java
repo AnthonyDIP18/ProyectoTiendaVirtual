55d292df80f7223d2a687c1d8dcdd371106f2008
Kenneth19920 <91219301+Kenneth19920@users.noreply.github.com>
2022-08-12 14:48:34 -0600

    Reconstruccion de la estructura del proyecto

--- src/main/java/com/TiendaP/TiendaPApplication.java
+++ src/main/java/com/TiendaP/TiendaPApplication.java
@@ -1,13 +1,13 @@
-package com.ProyectoTiendaVirtual.ProyectoTiendaVirtual;
+package com.TiendaP;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProyectoTiendaVirtualApplication {
+public class TiendaPApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProyectoTiendaVirtualApplication.class, args);
+		SpringApplication.run(TiendaPApplication.class, args);
                 
                 
                 
